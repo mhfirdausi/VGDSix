2c1611cfb6f72f47a3b48e343b23d9b241531fb1
Will <willjones1@gmail.com>
2016-02-17 17:26:20 -0600

    basic blocks and jumping player
    it was necessary to put colliders only on top of the blocks to keep
    movement steady and smooth. Causes pretty extreme clipping / not ideal.
    
    Colored block effects *should* just be able to change playerSpeed and
    jumpHeight variables

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+    public float playerSpeed;
+    private Vector3 dir;
+    public float jumpHeight = 18f;
+
+    public Rigidbody playerRigidBody;
+    private bool isFalling = false;
+
+    void Start () {
+        dir = Vector3.forward;
+	}
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.Space) && isFalling == false)
+        {
+            playerRigidBody.velocity = new Vector3(playerRigidBody.velocity.x, playerRigidBody.velocity.y + jumpHeight, playerRigidBody.velocity.y);
+        }
+
+        isFalling = true;
+
+        float distanceToMove = playerSpeed * Time.deltaTime;
+        transform.Translate(dir * distanceToMove);
+	}
+
+    void OnCollisionStay()
+    {
+        isFalling = false;
+    }
+}
