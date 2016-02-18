9e66a6ffd683fe65ced2719fb9a287002ba857ea
Will <willjones1@gmail.com>
2016-02-18 17:37:49 -0600

    Double Jump and faster move
    There is a bug with the way jumping interacts with colliding with
    blocks. (touching a block in any way reads as grounded, which allows
    another jump). Should be fixable by adding an invisible plate on top of
    each block for the character to collide with, instead of the actual
    visible block

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -3,31 +3,39 @@ using System.Collections;
 
 public class player : MonoBehaviour {
 
-    public float playerSpeed;
+    public float playerSpeed=50;
     private Vector3 dir;
-    public float jumpHeight = 18f;
+    public float jumpHeight = 70f;
 
     public Rigidbody playerRigidBody;
     private bool isFalling = false;
+    private int jumps;
 
     void Start () {
         dir = Vector3.forward;
 	}
 	
 	void Update () {
-        if (Input.GetKey(KeyCode.Space) && isFalling == false)
+        if (Input.GetKeyDown(KeyCode.Space) && (isFalling == false || jumps < 1))
         {
-            playerRigidBody.velocity = new Vector3(playerRigidBody.velocity.x, playerRigidBody.velocity.y + jumpHeight, playerRigidBody.velocity.y);
+            playerRigidBody.velocity = new Vector3(playerRigidBody.velocity.x,  jumpHeight, playerRigidBody.velocity.y);
+            jumps++;
         }
 
         isFalling = true;
 
+        
+	}
+
+    void FixedUpdate()
+    {
         float distanceToMove = playerSpeed * Time.deltaTime;
         transform.Translate(dir * distanceToMove);
-	}
+    }
 
     void OnCollisionStay()
     {
         isFalling = false;
+        jumps = 0;
     }
 }
