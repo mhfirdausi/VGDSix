9fc9489b41db6e93009c197f5c3930751a8bb959
Will <willjones1@gmail.com>
2016-02-18 22:22:26 -0600

    Introduces physics for boxes
    This solves the player clipping through boxes issue (which is compounded
    in our game due to the fact that the player can't ever slow down). It's
    also kinda neat. Easy to revert if you guys prefer it without this.

---
+++ Assets/Scripts/BoxFall.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxFall : MonoBehaviour {
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Awake () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter()
+    {
+        rb.useGravity = true;
+        rb.isKinematic = false;
+    }
+}
