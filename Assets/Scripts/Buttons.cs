e4b7678f19878697627146ab71ef02742ab28595
Turner Davis <tdavi67@lsu.edu>
2016-02-22 13:30:40 -0600

    Proof of concept for start and level select pages
    I want to know our direction for the design of these pages before
    moving forward.

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+	public Button startText;
+
+	void Start () {
+		startText = startText.GetComponent<Button> ();
+	}
+	
+	public void LoadLevelSelect() {
+		SceneManager.LoadScene ("LevelSelect");
+	}
+}
---
+++ Assets/Scripts/StartLevel.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartLevel : MonoBehaviour {
+
+	public Button beginButton;
+	public Button yeezyButton;
+	public Button futureButton;
+	public Canvas yeezyMenu;	
+	public Canvas futureMenu;
+
+	void Start () {
+		beginButton = beginButton.GetComponent<Button> ();
+		yeezyButton = yeezyButton.GetComponent<Button> ();
+		futureButton = futureButton.GetComponent<Button> ();
+		yeezyMenu = yeezyMenu.GetComponent<Canvas> (); 
+		futureMenu = futureMenu.GetComponent<Canvas> ();
+		yeezyMenu.enabled = false;
+		futureMenu.enabled = false;
+
+	}
+
+	public void yeezyPress() {
+		yeezyMenu.enabled = true;
+		futureMenu.enabled = false;
+	}
+
+	public void futurePress() {
+		yeezyMenu.enabled = false;
+		futureMenu.enabled = true;
+	}
+
+	public void loadYeezy()
+	{
+		SceneManager.LoadScene ("TLOP");
+
+	}
+
+	public void loadFuture()
+	{
+		SceneManager.LoadScene ("EVOL");
+
+	}
+}
