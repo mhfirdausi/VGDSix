eac62ae9fabc20b0e3d6add29ee38c9853bc6fbf
kmoise123 <kmoise123@hotmail.com>
2016-04-28 16:53:45 -0500

    teleport cheat
    only moves player a set units above its current y position.

---
+++ Assets/Scripts/teleportCheat.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teleportCheat : MonoBehaviour
+{
+
+    public GameObject checkpoint;
+    public player player;
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 x= new Vector3(player.transform.position.x, player.transform.position.y+40, player.transform.position.z);
+        checkpoint.transform.position = x;
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            player.transform.position = checkpoint.transform.position;
+        }
+	}
+}
