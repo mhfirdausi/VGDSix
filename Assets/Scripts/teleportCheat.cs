31cb0c1182897811019719860383a29f4b61779d
kmoise123 <kmoise123@hotmail.com>
2016-04-28 17:14:27 -0500

    key changed for cheat
    they key is now public; the key can be assigned in the unity editor.

--- Assets/Scripts/teleportCheat.cs
+++ Assets/Scripts/teleportCheat.cs
@@ -6,12 +6,13 @@ public class teleportCheat : MonoBehaviour
 
     public GameObject checkpoint;
     public player player;
+    public KeyCode key;
 	// Update is called once per frame
 	void Update ()
     {
         Vector3 x= new Vector3(player.transform.position.x, player.transform.position.y+40, player.transform.position.z);
         checkpoint.transform.position = x;
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(key))
         {
             player.transform.position = checkpoint.transform.position;
         }
