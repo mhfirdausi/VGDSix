08963cae8f4564123d105a5303a56cb8f4b1034a
Turner Davis <tdavi67@lsu.edu>
2016-04-29 12:12:55 -0500

    sprites

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,8 +7,14 @@ public class Timer : MonoBehaviour {
 	public Text timerLabel;
 	private float time;
 
+	public MusicLevelController musController;
+
+	void Awake()
+	{
+		musController = GameObject.FindGameObjectWithTag ("MusicController").GetComponent<MusicLevelController>();
+	}
 	void Update() {
-		StartCoroutine ("Wait");
+		TimeText ();
 	}
 
 	IEnumerator Wait()
@@ -20,5 +26,17 @@ public class Timer : MonoBehaviour {
 		var seconds = time % 60;
 
 		timerLabel.text = string.Format ("{0:0}:{1:00}/2:48", minutes, seconds);
+	}
+
+	void TimeText()
+	{
+		float currentTime = musController.currentSource.time;
+		float wholeTime = musController.currentSource.clip.length;
+		var minutes = currentTime / 60;
+		var seconds = currentTime % 60;
+		var minutesTotal = wholeTime / 60;
+		var secondsTotal = wholeTime % 60;
+
+		timerLabel.text = string.Format ("{0:0}:{1:00}/{0:2}:{3:00}", minutes, seconds, minutesTotal, secondsTotal);
 	}
 }
