62fef76bb3db587fc0f24757e4b91a0e45eba668
kmoise123 <kmoise123@hotmail.com>
2016-04-28 20:50:58 -0500

    cheat script and chaos level
    cheat script teleports player to checkpoint. the chaos level now works
    as intended.

---
+++ Assets/Scripts/RandomCube.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RandomCube : MonoBehaviour {
+
+
+    public GameObject neutral;
+    public GameObject slow;
+    public GameObject speed;
+    public GameObject bounce;
+    public GameObject player;
+    private int x;
+    private float timer;
+
+    void Awake()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        if (index != 5)
+        {
+            Destroy(this);
+        }
+    }
+    void Start()
+    {
+        timer = 0;
+        Vector3 x = player.transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        
+        if (timer > 5)
+        {
+            if (this.transform.position.z-10<=player.transform.position.z||this.transform.position.z+10>=player.transform.position.z) { 
+                x = UnityEngine.Random.Range(0, 10);
+                if (x == 1)
+                {
+                    GameObject obj = (GameObject)Instantiate(slow, neutral.transform.position, neutral.transform.rotation);
+                    Destroy(neutral);
+                    
+                }
+                else if (x == 2)
+                {
+                    GameObject obj = (GameObject)Instantiate(speed, neutral.transform.position, neutral.transform.rotation);
+                    Destroy(neutral);
+                                    }
+                else if (x == 3)
+                {
+                    GameObject obj = (GameObject)Instantiate(bounce, neutral.transform.position, neutral.transform.rotation);
+                    Destroy(neutral);
+                    
+
+                }
+                timer = 0;
+            }
+        }
+}
+}
+
--- Assets/Scripts/teleportCheat.cs
+++ Assets/Scripts/teleportCheat.cs
@@ -10,8 +10,6 @@ public class teleportCheat : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        Vector3 x= new Vector3(player.transform.position.x, player.transform.position.y+40, player.transform.position.z);
-        checkpoint.transform.position = x;
         if (Input.GetKeyDown(key))
         {
             player.transform.position = checkpoint.transform.position;
